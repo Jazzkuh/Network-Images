d69e7091f9d2a9ce818dde7a9e1080669bf16105
Esmaybe <esmaybe@revenity.net>
2024-05-10 19:45:11 +0200

    Add file

---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Print Java version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mjava -version\n"
+java -version
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# shellcheck disable=SC2086
+
+wget -r -np -q -nH --cut-dirs=2 --reject index.html,index.html.tmp "http://172.18.0.7/global/server" &
+wait $!
+
+if [ -n "$TEMPLATES" ]; then
+    IFS=',' read -r -a templates <<< "$TEMPLATES"
+    for template in "${templates[@]}"; do
+        wget -r -np -q -nH --cut-dirs=2 --reject index.html,index.html.tmp "http://172.18.0.7$template" &
+        wait $!
+    done
+else
+    echo "TEMPLATES environment variable is not set."
+fi
+
+sed -i "s/server-port=.*/server-port=$SERVER_PORT/" "/home/container/server.properties"
+
+exec env ${PARSED}
